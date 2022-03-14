667c4f787571bd379feb977d7c5d4f50154e1f1a
Sam Brierley <sam.brierley@qualitestgroup.com>
2022-03-14 15:09:00 +0000

    Added bar functionality to the App

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
-import "./NewExpense.css";
-import ExpenseForm from "./ExpenseForm";
+import React, { useState } from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
 
 const NewExpense = (props) => {
   const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -12,6 +14,7 @@ const NewExpense = (props) => {
     props.onAddExpense(expenseData);
     setIsEditing(false);
   };
+
   const startEditingHandler = () => {
     setIsEditing(true);
   };
@@ -19,8 +22,9 @@ const NewExpense = (props) => {
   const stopEditingHandler = () => {
     setIsEditing(false);
   };
+
   return (
-    <div className="new-expense">
+    <div className='new-expense'>
       {!isEditing && (
         <button onClick={startEditingHandler}>Add New Expense</button>
       )}
