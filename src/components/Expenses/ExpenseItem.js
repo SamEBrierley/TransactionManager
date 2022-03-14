00d9290f404dd79efc54f54a0b15d37b61b2b27c
Sam Brierley <sam.brierley@qualitestgroup.com>
2022-03-14 11:22:50 +0000

    Completed Section 4. Removed Unused code. Added a drop down menu to later filter transactions by year

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,19 +4,14 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem =(props) =>{
-  const [title, setTitle] = useState(props.title);
-  const clickedHandler = () =>{
-    setTitle("Updated");
-  }
 
   return (
     <Card className="expense-item">
     <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
-      </div>]
-      <button onClick={clickedHandler}>Change Title</button>
+      </div>
     </Card>
   );
 }
